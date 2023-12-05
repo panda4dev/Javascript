03a8728b6de9a45d7f1ca1290d65d1b9c74988bf
panda4dev <102847891+panda4dev@users.noreply.github.com>
2023-12-05 17:32:48 +0000

    study about operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -17,10 +17,40 @@ let valueinNumber = Number(newScore); // here if we check type of valueinNumber 
 // let valueinNumber = Number(nextScore); // type is number value is zero(0)
 // let valueinNumber = Number(newestScore); // type is number value is NaN
 
-
+let valueInBoolean = Boolean(score);
 // for boolean 
 // 1=> true    0 =. false 
 // empty string ""=> false 
 // non empty strings "shyam"=> true 
+
+// covert to string 
+
+let valueInString = String(score);
+
+
+
+// ******************************OPERATIONS*******************************************************
+
+let value = 3
+let negvalue = -value;
+console.log(negvalue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/2);
+console.log(5%2);
+
+// important behaviour 
+console.log("1" + 2);  // output is 12
+console.log(1 + "2");  // output is 12
+console.log("1" + 2 + 2); // output is 122
+console.log(1 + 2 + "2"); // output is 32
+
+console.log(+true) // output is 1
+
+
+
 
 
