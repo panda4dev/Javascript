033992bf4bcf1b8e4d68e4dc50f20b45954cd38e
panda4dev <102847891+panda4dev@users.noreply.github.com>
2023-12-05 16:37:37 +0000

    this file is about data types in js

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all js code as newer version 
+
+//alert(3 + 3)  // doesnt work because we are using nodejs not browser
+
+//different data types
+
+//number 
+// bigint
+//string
+//boolean 
+//null => standalone value
+//undefined => abhi value aasigned nhi hui hai 
+//symbol => uniqueness ( to find uniqueness mostly used in react to find unique components in react )
+
+//these are primitive datatypes 
+
+
+//objects
+
+console.log(typeof undefined); //undefined 
+console.log(typeof null) // object
