c52d9eedc74326fd8030d7a0bbb81a230a2eabe6
panda4dev <102847891+panda4dev@users.noreply.github.com>
2023-12-08 08:08:36 +0000

    reduce method

---
+++ 05_iterations/nine.js
@@ -0,0 +1,40 @@
+
+// reduce 
+
+const myNums = [1, 2, 3]
+
+// const myTotal = myNums.reduce(function (acc, currval) {
+//     console.log(`acc: ${acc} and currval: ${currval}`);
+//     return acc + currval
+// }, 0)
+
+// at start accumulator is assgned with initialvalue and then sum of acc + current value is assigned to the accumulator 
+
+// value after comma "," is called initial value 
+const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)
+
+console.log(myTotal);
+
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+
+console.log(priceToPay);
