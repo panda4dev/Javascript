54c392216ae3cab9ec551216d5f0f091e8468b6f
panda4dev <102847891+panda4dev@users.noreply.github.com>
2024-02-15 13:40:29 +0000

    Project one completed

---
+++ 07_BasicProjects/FirstProj/index.js
@@ -0,0 +1,23 @@
+const buttons = document.querySelectorall('.button');
+const body = document.querySelector('body');
+buttons.forEach(button => {
+
+        console.log(button) // to see all selected buttons 
+
+    button.addEventListener('onClick',function (e) {
+        console.log(e);
+        console.log(e.target);
+        if (e.target.id === 'grey') {
+          body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === 'white') {
+          body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === 'blue') {
+          body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === 'yellow') {
+          body.style.backgroundColor = e.target.id;
+        } 
+    });
+});
